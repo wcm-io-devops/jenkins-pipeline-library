9ce7cf85d02b2fa0c168853107f5df2015e4652e
Tobias Richter <trichter@pro-vision.de>
2018-09-24 11:40:51 +0200

    Optimized loadAndExecuteScript

--- test/io/wcm/testing/jenkins/pipeline/LibraryIntegrationTestBase.groovy
+++ test/io/wcm/testing/jenkins/pipeline/LibraryIntegrationTestBase.groovy
@@ -309,36 +309,10 @@ class LibraryIntegrationTestBase extends BasePipelineTest {
    * 'afterLoadingScript' is executed afterwards
    *
    * @param scriptPath The Path to the test job
-   * @return The return value of the executed script
-   */
-  protected loadAndExecuteScript(String scriptPath) {
-    def ret
-    try {
-      // call helper function to enable tests to execute code before loading the script
-      beforeLoadingScript()
-      def script = loadScript(scriptPath)
-      // call helper function to enable tests to redirect pipeline steps into own callbacks
-      afterLoadingScript()
-      ret = script.execute()
-    } catch (e) {
-      e.printStackTrace()
-      this.context.getDslMock().printLogMessages()
-      throw e
-    }
-    return ret
-  }
-
-  /**
-   * Utility function to load and execute a script (e.g. test pipeline)
-   * The function calls the utility function 'beforeLoadingScript' before loading the script via the JenkinsPipelineUnit
-   * framework. To enable the test to redirect executed steps defined in the implicitely loaded library the function
-   * 'afterLoadingScript' is executed afterwards
-   *
-   * @param scriptPath The Path to the test job
    * @param config This map will be passed to the execute function of the script
    * @return The return value of the executed script
    */
-  protected loadAndExecuteScript(String scriptPath, Map config) {
+  protected loadAndExecuteScript(String scriptPath, Map config = null) {
     def ret
     try {
       // call helper function to enable tests to execute code before loading the script
@@ -346,7 +320,11 @@ class LibraryIntegrationTestBase extends BasePipelineTest {
       def script = loadScript(scriptPath)
       // call helper function to enable tests to redirect pipeline steps into own callbacks
       afterLoadingScript()
-      ret = script.execute(config)
+      if (config != null) {
+        ret = script.execute(config)
+      } else {
+        ret = script.execute()
+      }
     } catch (e) {
       e.printStackTrace()
       this.context.getDslMock().printLogMessages()
